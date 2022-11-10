3747f3b90e811b10526e527f96e9b4b6660c1819
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-11-10 12:24:43 +0100

    starting with recharts

---
+++ dashboard/src/components/chart/Chart.jsx
@@ -0,0 +1,9 @@
+import './chart.css'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+export default function Chart() {
+  return (
+    <div className='chart'>
+      <h3 className="chartTitle">Sales Insights</h3>
+    </div>
+  )
+}
--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Chart from '../components/chart/Chart'
 import FeaturedInfo from '../components/featuredInfo/FeaturedInfo'
 import './home.css'
 export default function Home() {
   return (
     <div className='home'>
-      <FeaturedInfo/></div>
+      <FeaturedInfo/>
+      <Chart/>
+      </div>
   )
 }
